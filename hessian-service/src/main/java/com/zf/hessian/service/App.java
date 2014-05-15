1cddc8d5e91003b357208bbc547290baa3838be7
is_zhoufeng <476347510@qq.com>
2014-05-15 14:48:35 +0800

    init

---
+++ hessian-service/src/main/java/com/zf/hessian/service/App.java
@@ -0,0 +1,13 @@
+package com.zf.hessian.service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
